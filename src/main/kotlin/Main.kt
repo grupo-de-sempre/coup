87ce3c4114e0ee1e522bacdcd00d6440c5f1c95e
Highlander Paiva <31507490+hvpaiva@users.noreply.github.com>
2023-03-31 15:08:19 -0300

    refactor: updated code to be more concise and follow design needed (#1)
    * refactor: updated code to be more concise and follow  design needed
    
    * refactor: updated the checkPlayerInfluence to receive a player besides iterate the whole player list

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,1 +1,30 @@
-fun main() { }
+import org.coup.game.actions.*
+import org.coup.game.actions.ActionMediator
+
+fun main() {
+    val elder = Player("Elder")
+    val high = Player("Highlander")
+    val game = CoupGame(mutableListOf(elder, high))
+    val mediator = ActionMediator()
+
+    mediator.addAction(Steal(dispatcher = elder, target = high))
+    mediator.addAction(CounterSteal(dispatcher = high))
+    mediator.addAction(Challenge(dispatcher = elder, target = high))
+
+    mediator.resolveAsSuccess(game)
+
+    println("--------------------")
+
+    mediator.addAction(Tax(dispatcher = high))
+    mediator.addAction(Challenge(dispatcher = elder, target = high))
+
+    mediator.resolveAsFailure(game)
+
+    println("--------------------")
+
+    mediator.addAction(Assassinate(dispatcher = high, target = elder))
+    mediator.addAction(CounterAssassinate(dispatcher = elder))
+    mediator.addAction(Challenge(dispatcher = high, target = elder))
+
+    mediator.resolveAsSuccess(game)
+}
--- src/main/kotlin/org/coup/game/actions/CoupGame.kt
+++ src/main/kotlin/org/coup/game/actions/CoupGame.kt
@@ -1,23 +1,82 @@
 package org.coup.game.actions
 
+/**
+ * This class represents a game of Coup.
+ *
+ * It is responsible for managing the game state and the players.
+ *
+ * @param players The players in the game
+ * @property bank The number of coins in the bank
+ * @property deck The deck of cards
+ * @constructor Creates a new game of Coup, giving each [Player] 2 coins and 2 influences.
+ */
 class CoupGame(
     private var players: MutableList<Player> = mutableListOf()
 ) {
-    private var bankCoins = 50
+    private var bank = 50
+    private var deck = null;
 
     init {
-        players.forEach { it.incrementCoins(2) }
+        players.forEach { player ->
+            incrementCoins(player, 2)
+            player.addInfluence(2)
+        }
     }
 
+    /**
+     * Gives a [player] coins from the [bank]. Given the [amount] of coins.
+     *
+     * It will give the player the maximum amount of coins possible. If the [bank]
+     * has fewer coins than the [amount], it will give the [player] all the coins
+     * in the [bank].
+     *
+     * @param player The player to give coins to
+     * @param amount The amount of coins to give
+     */
     fun incrementCoins(player: Player, amount: Int) {
-        val coins = minOf(amount, bankCoins)
+        val coins = minOf(amount, bank)
         player.incrementCoins(coins)
-        bankCoins -= coins
+        bank -= coins
     }
 
+    /**
+     * Takes a [player] coins and put it in the [bank]. Given the [amount] of coins.
+     *
+     * It will take the player the maximum amount of coins possible. If the [player]
+     * has fewer coins than the [amount], it will take all the coins from the [player].
+     *
+     * @param player The player to take coins from
+     * @param amount The amount of coins to take
+     */
     fun decrementCoins(player: Player, amount: Int) {
         val coins = minOf(amount, player.coins)
         player.decrementCoins(coins)
-        bankCoins += coins
+        bank += coins
+    }
+
+    /**
+     * Removes an influence from a [player].
+     *
+     * If the [player] has no more influences, it will be eliminated from the game.
+     *
+     * @param player The player to remove an influence from
+     * @see checkPlayerInfluence
+     */
+    fun removeInfluence(player: Player) {
+        println("${player.name} lost an influence")
+        player.removeInfluence(1)
+        checkPlayerInfluence(player)
+    }
+
+    /**
+     * Checks if a [player] has no more influences. If so, they are eliminated from the game.
+     *
+     * @param player The player to check
+     */
+    private fun checkPlayerInfluence(player: Player) {
+        if (player.influence == 0) {
+            println("${player.name} lost the game")
+            players.remove(player)
+        }
     }
 }
