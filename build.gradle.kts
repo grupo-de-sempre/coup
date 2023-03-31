95d0366762c467cc1191a20a8041427e96f0a71b
Highlander Paiva <31507490+hvpaiva@users.noreply.github.com>
2023-03-31 15:39:17 -0300

    build: added jacoco support to enable code coverage (#2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.8.0"
     application
+    jacoco
 }
 
 group = "dev.gds"
@@ -16,10 +17,32 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 kotlin {
     jvmToolchain(8)
+}
+
+tasks.jacocoTestReport {
+    dependsOn("test")
+
+    executionData.setFrom(fileTree(buildDir.resolve("jacoco")).apply {
+        include("*.exec", "*.ec")
+    })
+    classDirectories.setFrom(classDirectories.files.map {
+        fileTree(it).matching {
+            exclude()
+        }
+    })
+
+    sourceDirectories.setFrom(files("src/main/kotlin"))
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHml"))
+    }
 }
 
 application {
